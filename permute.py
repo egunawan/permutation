96ad8776bd656af3df9ef2b920a25ee6a038a691
Emily Gunawan <egunawan@umn.edu>
2016-06-25 18:57:39 -0500

    change iter to use nayuki.io code

--- permute.py
+++ permute.py
@@ -94,21 +94,6 @@ class myobj(ClonableArray):
         return matrix(n, entries, sparse = True)
 
 
-class mytranspositions(myobjs):
-    """
-    Permutations on [n] which swaps exactly two numbers
-    """
-    def __iter__(self):
-        line = range(1,self._n+1)
-        for i in range(0,self._n+1):
-            for j in range(i+1,self._n):
-                line[i],line[j]=j+1,i+1
-                yield self.element_class(self,line)
-                line[i],line[j]=i+1,j+1
-
-    def __repr__(self):
-        return 'Transposition permutations on [{}]'.format(self._n)
-
 class myobjs(Parent,UniqueRepresentation):
     """
     Permutations on [n]
@@ -123,6 +108,12 @@ class myobjs(Parent,UniqueRepresentation):
     Element = myobj
 
     def __iter__(self):
+       perm = range(1,self._n+1) # identity permutation
+       yield self.element_class(self,perm)
+       while next_permutation(perm):
+          yield self.element_class(self,perm)
+
+    def bak__iter__(self):
         s=range(1,self._n+1)
         list=s
         used = [False] * len(list)
@@ -134,31 +125,74 @@ class myobjs(Parent,UniqueRepresentation):
     def n(self):
         return self._n
 
-def do_permute(list, out, used, level):
+class mytranspositions(myobjs):
     """
-    Used to produce all permutations
+    Permutations on [n] which swaps exactly two numbers.
+    Not ordered by lexicographic order
     """
-    if len(list) == level:
-		yield out
-    else:
-        for i in range(0, len(list)):
-            if used[i]:
-                continue
-            used[i] = True
-            for perm in do_permute(list, out + [list[i]], used, level + 1):
-                #print 'perm:',perm
-                yield perm
-            used[i] = False
+    def __iter__(self):
+        line = range(1,self._n+1)
+        for i in range(0,self._n+1):
+            for j in range(i+1,self._n):
+                line[i],line[j]=j+1,i+1
+                yield self.element_class(self,line)
+                line[i],line[j]=i+1,j+1
+
+    def __repr__(self):
+        return 'Transposition permutations on [{}]'.format(self._n)
+
+def next_permutation(arr):
+    """
+    https://www.nayuki.io/res/next-lexicographical-permutation-algorithm/nextperm.py
+    """
+    # Find non-increasing suffix
+    i = len(arr) - 1
+    while i > 0 and arr[i - 1] >= arr[i]:
+        i -= 1
+    if i <= 0:
+        return False
+
+    # Find successor to pivot
+    j = len(arr) - 1
+    while arr[j] <= arr[i - 1]:
+        j -= 1
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+
+    # Reverse suffix
+    arr[i : ] = arr[len(arr) - 1 : i - 1 : -1]
+    return True
+
+
 
 ###################### start test ########################
-def test_myobjs():
-    M = myobjs(5)
-    if not M([5,4,3,2,1]).compose(M([5,4,3,2,1]))._line == M([1,2,3,4,5])._line:
+def test_myobjs(n=5):
+    M = myobjs(n)
+    if not len(M) == factorial(n):
+        print 'len(M):',len(M)
         return False
-    if not len(mytranspositions(10))==10*9/2:
+    if not myobj([5,4,3,2,1]).compose(myobj([5,4,3,2,1]))._line == myobj([1,2,3,4,5])._line:
+        print 'myobj([5,4,3,2,1]).compose(myobj([5,4,3,2,1]))._line:', myobj([5,4,3,2,1]).compose(myobj([5,4,3,2,1]))._line
+        return False
+    if not len(mytranspositions(n))==n*(n-1)/2:
+        print 'len(mytranspositions(n))',len(mytranspositions(n))
         return False
     if not myobj([5,4,1,2,3,6]).to_matrix() == Permutation([5,4,1,2,3,6]).to_matrix():
         return False
     return True
 
 
+#def do_permute(list, out, used, level):
+#    """
+#    Used to produce all permutations
+#    """
+#    if len(list) == level:#
+#		yield out
+#    else:
+#        for i in range(0, len(list)):
+#            if used[i]:
+#                continue
+#            used[i] = True
+#            for perm in do_permute(list, out + [list[i]], used, level + 1):
+#                #print 'perm:',perm
+#                yield perm
+#            used[i] = False
